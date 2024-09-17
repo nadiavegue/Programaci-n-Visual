c6eb3c0b335aa2fd66ef3c9b8ed1f9226aa9c8d5
nadiavegue <145699648+nadiavegue@users.noreply.github.com>
2024-09-17 15:41:35 +0200

    Add files via upload

---
+++ AcercaDe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reloj
+{
+    public partial class AcercaDe : Form
+    {
+        public AcercaDe()
+        {
+            InitializeComponent();
+
+  
+        }
+
+        private void AcercaDe_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
